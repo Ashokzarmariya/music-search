cda5929a351a893b5aaa01417b51ec7b20c36fb3
ashokzarmariyaa <ashok.zarmariya@permissionles.org>
2023-02-24 22:51:27 +0530

    create search music function

--- src/Pages/Search/Search.tsx
+++ src/Pages/Search/Search.tsx
@@ -1,10 +1,38 @@
 import React from "react";
+// import { useDispatch } from "react-redux";
+import { useDispatch } from 'react-redux';
+
+import { searchMusicHandler } from "../../Redux/song/Action";
 import "./Search.css";
 
 const Search = () => {
+  const dispatch=useDispatch();
+
   const handleChange = (keyword: string) => {
     console.log("search --- ", keyword);
+    searchHandler(keyword)
   };
+
+  const searchHandler= async (keyword:string)=>{
+
+    // https://shazam.p.rapidapi.com/search?term=kiss%20the%20rain&locale=en-US&offset=0&limit=5
+    const res = await fetch(
+      `https://shazam.p.rapidapi.com/search?term=${keyword}&offset=0&limit=5`,
+      {
+        method: "GET",
+        headers: {
+            'X-RapidAPI-Key': '1da9632d82mshe52cacb569a7414p138479jsn8498ceeb2ea5',
+            'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+        }
+      }
+    );
+
+    console.log("res ",res)
+  
+    const data = await res.json();
+    
+    console.log("search result ", data)
+  }
 
   return (
     <div>
--- src/Redux/song/Action.ts
+++ src/Redux/song/Action.ts
@@ -1,21 +1,36 @@
 import { SEARCH_SONG } from "./ActionType";
+import { ThunkAction } from 'redux-thunk';
+// import { RootState } from './store';
+import { AnyAction, Store } from 'redux';
 
-export const searchMusicHandler = (keyword:string) => async (dispatch:any) => {
+
+export const searchMusicHandler = (keyword:string)=> async (dispatch:any) => {
     
-    const res = await fetch(
-      `https://shazam.p.rapidapi.com/auto-complete?term=${keyword}`,
-      {
-        method: "GET",
-        headers: {
-            'X-RapidAPI-Key': '1da9632d82mshe52cacb569a7414p138479jsn8498ceeb2ea5',
-            'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
-        }
-      }
-    );
+
+    try {
+
+        const res = await fetch(
+            `https://shazam.p.rapidapi.com/auto-complete?term=${keyword}`,
+            {
+              method: "GET",
+              headers: {
+                  'X-RapidAPI-Key': '1da9632d82mshe52cacb569a7414p138479jsn8498ceeb2ea5',
+                  'X-RapidAPI-Host': 'shazam.p.rapidapi.com'
+              }
+            }
+          );
+        
+         
+          const data = await res.json();
+          
+          console.log("search result ", data)
+      
+          dispatch({ type: SEARCH_SONG, payload:data });
+        
+    } catch (error) {
+        // dispatch({ type: SEARCH_SONG, payload:error.message});
+        console.log("error ",error)
+    }
+
   
-   
-    const data = await res.json();
-    
-    
-    dispatch({ type: SEARCH_SONG, payload:data });
 };
