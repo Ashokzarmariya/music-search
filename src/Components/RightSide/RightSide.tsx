d4cf1a58689e20eb0314976a2e58fbea996a4bfa
ashokzarmariyaa <ashok.zarmariya@permissionles.org>
2023-02-27 10:17:56 +0530

    add fav song in home page

--- src/Components/RightSide/RightSide.tsx
+++ src/Components/RightSide/RightSide.tsx
@@ -1,12 +1,29 @@
-import React from "react";
-import Favorite from "../FavoriteSong/FavoriteSong";
+import React, { useEffect, useState } from "react";
+import Favorite, { song } from "../FavoriteSong/FavoriteSong";
 import PlaylistCard from "../PlaylistCard/PlaylistCard";
 import Song from "../Songs/Song";
 import UserPlaylist from "../UserPlaylist/UserPlaylist";
 import "./RightSide.css";
 
 const RightSide = () => {
-  const songs=[{track:{title:"aaa",album:"bbbb",duration:"10 hours",images:{background:"https://tse2.mm.bing.net/th?id=OIP.khdcOD-9zfajSweZVrGfJwHaEP&pid=Api&P=0"}}}];  return (
+
+  const songs=[{track:{title:"aaa",album:"bbbb",duration:"10 hours",images:{background:"https://tse2.mm.bing.net/th?id=OIP.khdcOD-9zfajSweZVrGfJwHaEP&pid=Api&P=0"}}}]; 
+
+  const [favorite, setFavorite] = useState<song[]>([]);
+  const favoriteSongs = localStorage.getItem("favorite_songs");
+
+  useEffect(() => {
+    if (favoriteSongs) {
+      console.log(
+        "local storage ------------------ ",
+        JSON.parse(favoriteSongs)
+      );
+      setFavorite(JSON.parse(favoriteSongs));
+    }
+  }, [favoriteSongs]);
+  
+  
+  return (
     <div>
       <div className="userPlaylist">
         <h1 className="heading">My Playlist</h1>
@@ -19,7 +36,7 @@ const RightSide = () => {
         <h1 className="heading">Favourite Song</h1>
 
         <div>
-         {/* <Favorite data={songs}/> */}
+         <Favorite isShow={false} data={favorite}/>
         </div>
       </div>
     </div>
