686c82edcff4296c4546d3989b0a114b1dea1c8d
ashokzarmariyaa <ashok.zarmariya@permissionles.org>
2023-02-27 10:27:18 +0530

    fixed palylist item font size

--- src/Components/AddSongToPlaylist/AddSongToPlaylistModal.tsx
+++ src/Components/AddSongToPlaylist/AddSongToPlaylistModal.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Button, Modal } from "react-bootstrap";
 import { playlist } from "../../Configs/Interfaces";
 import { song } from "../FavoriteSong/FavoriteSong";
+import "./AddSongToPlaylist.css";
 
 interface props {
   showModal: boolean;
@@ -45,7 +46,7 @@ const AddSongToPlaylistModal = ({ showModal, handleCloseModal,song }: props) => 
             {playlist.map((item) => (
               <p
                 onClick={() => handleAddSongToPlaylist(item?.title)}
-                className="px-2 p-1 bg-primary my-1 text-white text-capitalize"
+                className="px-2 p-1 bg-primary my-1 text-white text-capitalize playlistItem"
               >
                 {item.title}
               </p>
